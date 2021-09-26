4e12d458765948d0fd5a1047621f14129a2dc0d7
ratsstar <89663576+ratsstar@users.noreply.github.com>
2021-09-26 13:04:31 +0300

    Add files via upload

--- analysis.py
+++ analysis.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 # write your code here
 # After all the libraries imports write the following line of code
-pd.set_option('display.max_columns', 8)
+# pd.set_option('display.max_columns', 8)
 # Read the CSV files with datasets
 general = pd.read_csv('general.csv')
 prenatal = pd.read_csv('prenatal.csv')
@@ -28,7 +28,64 @@ df['gender'] = df['gender'].fillna('f')
 c = ['bmi', 'diagnosis', 'blood_test', 'ecg', 'ultrasound', 'mri', 'xray', 'children', 'months']
 df[c] = df[c].fillna(0)
 # Print shape of the resulting data frame
-print(df.shape)
+# print(df.shape)
 # Print random 20 rows of the resulting data frame. For the reproducible output set random_state=30
-print(df.sample(n=20, random_state=30))
+# print(df.sample(n=5, random_state=30))
 # print(df.index)
+# Answer the following questions and output the answers in the specified format.
+
+# 1. Which hospital has the highest number of patients?
+# hospitals = df.hospital.unique()
+# print(len(hospitals))
+# print(hospitals)
+# print(df.hospital.value_counts().general)
+# df.groupby('hospital').count()
+# print(df.groupby('hospital').count())
+stat = dict(df.groupby('hospital').count().gender)
+hospitals = list(stat.keys())
+patients = list(stat.values())
+# print(max(patients))
+# print(patients.index(max(patients)))
+# print(hospitals[patients.index(max(patients))])
+answer_1 = hospitals[patients.index(max(patients))]
+
+# 2. What share of the patients in the general hospital suffers from stomach-related issues? Round the
+# result to the third decimal place.
+patients_general = (stat['general'])
+# print(df.columns.values)
+# hospital_general = df.loc[df['hospital'] == 'general']
+# print(hospital_general)
+general_stomach = df.loc[(df['hospital'] == 'general') & (df['diagnosis'] == 'stomach')].shape[0]
+answer_2 = round(general_stomach / patients_general, 3)
+# print(answer_2)
+# 3. What share of the patients in the sports hospital suffers from dislocation-related issues?
+patients_sports = (stat['sports'])
+sports_dislocation = df.loc[(df['hospital'] == 'sports') & (df['diagnosis'] == 'dislocation')].shape[0]
+answer_3 = round(sports_dislocation / patients_sports, 3)
+# Round the result to the third decimal place.
+
+# 4. What is the difference in the median ages of the patients in the general and sports hospitals?
+general_median_age = df.loc[(df['hospital'] == 'general')].age.median()
+sports_median_age = df.loc[(df['hospital'] == 'sports')].age.median()
+answer_4 = int(abs(general_median_age - sports_median_age))
+
+# 5. After data processing at the previous stages, the blood_test column has three values:
+# t= a blood test was taken, f= a blood test wasn't taken, and 0= there is no information.'
+# In which hospital the blood test was taken the most often (there is the biggest number of t
+# in the blood_test column among all the hospitals)? How many blood tests were taken?
+blood_test_taken = df.loc[df['blood_test'] == 't']
+blood_stat = dict(blood_test_taken.groupby('hospital').count().gender)
+hospitals = list(blood_stat.keys())
+blood_stat_number = list(blood_stat.values())
+
+# print(max(patients))
+# print(patients.index(max(patients)))
+# print(hospitals[patients.index(max(patients))])
+answer_52 = max(blood_stat_number)
+answer_51 = hospitals[blood_stat_number.index(answer_52)]
+
+print(f'''The answer to the 1st question is {answer_1}
+The answer to the 2nd question is {answer_2}
+The answer to the 3rd question is {answer_3}
+The answer to the 4th question is {answer_4}
+The answer to the 5th question is {answer_51}, {answer_52} blood tests''')
